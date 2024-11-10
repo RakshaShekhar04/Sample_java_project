1b3ed8d9aa6366bd4be61b302b8581d7e41bf817
RakshaShekhar07 <154491318+RakshaShekhar07@users.noreply.github.com>
2024-11-10 08:35:10 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -3,5 +3,6 @@ public class Demo{
     System.out.println ("Hello from Demo");
         System.out.println ("Hello to Sweta from demo java");
          System.out.println ("Hello to all from demo java");
+         System.out.println ("Hello everyone from demo java");
     }
 }
