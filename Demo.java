29682311182278c71249fe92bd318af082f7d427
RakshaShekhar07 <154491318+RakshaShekhar07@users.noreply.github.com>
2025-03-10 18:34:22 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -2,5 +2,6 @@ public class Demo{
     public static void main (String [] args){
     System.out.println ("Hello from Demo");
          System.out.println ("Hello everyone from demo java");
+         System.out.println ("Updating the code");
     }
 }
