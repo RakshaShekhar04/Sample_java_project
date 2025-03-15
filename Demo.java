e1c24d9756a8dec32b5b1860c3ecd3a9f739ee4f
RakshaShekhar07 <154491318+RakshaShekhar07@users.noreply.github.com>
2025-03-15 08:20:57 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -2,8 +2,6 @@ public class Demo{
     public static void main (String [] args){
     System.out.println ("Hello from Demo");
          System.out.println ("Hello everyone from demo java");
-         System.out.println ("Updating the code");
-        System.out.println ("Updating the code");
-         System.out.println ("Updating the code");
+         System.out.println ("Used webhook");
     }
 }
