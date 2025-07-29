cd6304f12bffb892d7ce643cc335cfe06d4832b6
RakshaShekhar04 <vignetteverseful16@gmail.com>
2025-07-29 11:19:42 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -2,5 +2,6 @@ public class Demo{
     public static void main (String [] args){
     System.out.println ("Hello from Demo");
          System.out.println ("Hello everyone from demo java here");
+        System.out.println ("Hello Sri & Asad ");
     }
 }
